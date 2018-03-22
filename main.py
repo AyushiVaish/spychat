0a7c3adc7f0f9c0ba2939a7a5032e26cf052d407
Ayushi Vaish <ayushimalaiwa99@gmail.com>
2018-03-22 22:06:22 +0530

    Spychat first project

---
+++ main.py
@@ -0,0 +1,7 @@
+print 'hello'
+print 'what\'s up?'
+spy_name=raw_input("What is your name? ")
+print 'Welcome ' + spy_name + '.Glad to see you back. '
+spy_salutation=raw_input('What should we call you(Mr.or Ms.)?')
+print spy_salutation + " "+ spy_name
+
